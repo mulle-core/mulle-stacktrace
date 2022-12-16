ecc96327bbc3d263fde4eecfca3bc15765bc92e0
Nat! <nat@mulle-kybernetik.com>
2022-12-16 15:15:57 +0100

    fix CI settings

--- .mulle/etc/env/environment-host-ci-prerelease.sh
+++ .mulle/etc/env/environment-host-ci-prerelease.sh
@@ -1,54 +1,54 @@
 #
-# mulle-dlfcn nodetype: ${MULLE_DLFCN_NODETYPE:-tar}
+# mulle-dlfcn nodetype: ${MULLE__DLFCN_NODETYPE:-tar}
 #
-export MULLE_DLFCN_NODETYPE="git"
+export MULLE__DLFCN_NODETYPE="git"
 
 
 #
-# mulle-dlfcn url: ${MULLE_DLFCN_URL:-https://github.com/mulle-core/mulle-dlfcn/archive/${MULLE_TAG}.tar.gz}
+# mulle-dlfcn url: ${MULLE__DLFCN_URL:-https://github.com/mulle-core/mulle-dlfcn/archive/${MULLE_TAG}.tar.gz}
 #
-export MULLE_DLFCN_URL="https://github.com/mulle-core/mulle-dlfcn.git"
+export MULLE__DLFCN_URL="https://github.com/mulle-core/mulle-dlfcn.git"
 
 
 #
-# mulle-dlfcn branch: ${MULLE_DLFCN_BRANCH}
+# mulle-dlfcn branch: ${MULLE__DLFCN_BRANCH}
 #
-export MULLE_DLFCN_BRANCH="prerelease"
+export MULLE__DLFCN_BRANCH="prerelease"
 
 
 #
-# mulle-c11 nodetype: ${MULLE_C11_NODETYPE:-tar}
+# mulle-c11 nodetype: ${MULLE__C11_NODETYPE:-tar}
 #
-export MULLE_C11_NODETYPE="git"
+export MULLE__C11_NODETYPE="git"
 
 
 #
-# mulle-c11 url: ${MULLE_C11_URL:-https://github.com/mulle-c/mulle-c11/archive/${MULLE_TAG}.tar.gz}
+# mulle-c11 url: ${MULLE__C11_URL:-https://github.com/mulle-c/mulle-c11/archive/${MULLE_TAG}.tar.gz}
 #
-export MULLE_C11_URL="https://github.com/mulle-c/mulle-c11.git"
+export MULLE__C11_URL="https://github.com/mulle-c/mulle-c11.git"
 
 
 #
-# mulle-c11 branch: ${MULLE_C11_BRANCH}
+# mulle-c11 branch: ${MULLE__C11_BRANCH}
 #
-export MULLE_C11_BRANCH="prerelease"
+export MULLE__C11_BRANCH="prerelease"
 
 
 #
-# dlfcn-win32 nodetype: ${DLFCN_WIN32_NODETYPE:-tar}
+# dlfcn-win32 nodetype: ${DLFCN__WIN32_NODETYPE:-tar}
 #
-export DLFCN_WIN32_NODETYPE="git"
+export DLFCN__WIN32_NODETYPE="git"
 
 
 #
-# dlfcn-win32 url: ${DLFCN_WIN32_URL:-https://github.com/mulle-core/dlfcn-win32/archive/refs/tags/${MULLE_TAG}.tar.gz}
+# dlfcn-win32 url: ${DLFCN__WIN32_URL:-https://github.com/mulle-core/dlfcn-win32/archive/refs/tags/${MULLE_TAG}.tar.gz}
 #
-export DLFCN_WIN32_URL="https://github.com/mulle-core/dlfcn-win32.git"
+export DLFCN__WIN32_URL="https://github.com/mulle-core/dlfcn-win32.git"
 
 
 #
-# dlfcn-win32 branch: ${DLFCN_WIN32_BRANCH}
+# dlfcn-win32 branch: ${DLFCN__WIN32_BRANCH}
 #
-export DLFCN_WIN32_BRANCH="prerelease"
+export DLFCN__WIN32_BRANCH="prerelease"
 
 
